9340fb7b0184edd47ccb38c3c4d23192b8591f36
Nghiem Van Long <88993999+oniyatos@users.noreply.github.com>
2022-11-03 15:03:45 +0700

    Add files via upload

---
+++ RockiesTest/PageObject/LoginPage.cs
@@ -0,0 +1,26 @@
+﻿
+using OpenQA.Selenium;
+using CoreFrameWork.DriverCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreFramework.DriverCore;
+
+namespace RookieTest.PageObject
+{
+    public class LoginPage : WebDriverAction
+    {
+        public LoginPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        private readonly String tfUsername = "//input[@name='uid']";
+
+        public void InputUserName(String UserName)
+        {
+            SendKeys_(tfUsername, UserName);
+        }
+    }
+}
---
+++ RockiesTest/TestSetUp/TestSetUp.cs
@@ -0,0 +1,37 @@
+﻿using CoreFramework.DriverCore;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+
+namespace RookiesTest.TestSetup
+{ 
+public class NUnitTestSetup : WebDriverManager
+{
+    public IWebDriver? driver;
+    public WebDriverAction? driverBaseAction;
+
+    [SetUp]
+    public void Setup()
+    {
+        WebDriverManager.InitDriver("chrome", 1920, 1080);
+        driver = WebDriverManager.GetCurrentDriver();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        // _driver?.Quit();
+        TestStatus testStatus = TestContext.CurrentContext.Result.Outcome.Status;
+        if (testStatus.Equals(TestStatus.Passed))
+        {
+            TestContext.WriteLine("Passed");
+        }
+        else if (testStatus.Equals(TestStatus.Failed))
+        {
+            TestContext.WriteLine("Failed");
+                driverBaseAction.CapturedScreen();
+        }
+    }
+
+}
+}
