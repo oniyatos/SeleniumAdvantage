7fe6b13ec542e75d9178fd93fbd9c36fea42970f
Nghiem Van Long <88993999+oniyatos@users.noreply.github.com>
2022-11-14 09:23:50 +0700

    Update for API Request/Response and fix reporter
    Still missing Test Case for API  and mark up, will update later on

---
+++ SeleniumAdvantage_Day3/OneforAllFrameWork/APICore/APIRequest.cs
@@ -0,0 +1,80 @@
+﻿using System.Net;
+using System.ServiceModel;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace OneforAllFrameWork
+{
+    public static class Helper
+    {
+        private static BasicHttpBinding _basicHttpBinding;
+        private static RestRequest _restRequest;
+        private static RestResponse _restResponse;
+
+
+        public static string DeserializeResponseUsingJObject(this RestResponse restResponse, string responseObj)
+        {
+            var jObject = JObject.Parse(restResponse.Content);
+            return jObject[responseObj]?.ToString();
+        }
+
+        public static bool IsSuccessStatusCode(this HttpStatusCode responseCode)
+        {
+            var numericResponse = (int)responseCode;
+
+            const int statusCodeOk = (int)HttpStatusCode.OK;
+
+            const int statusCodeBadRequest = (int)HttpStatusCode.BadRequest;
+
+            return numericResponse >= statusCodeOk &&
+                   numericResponse < statusCodeBadRequest;
+        }
+
+        public static RestRequest CreateGetRequest(string optionalEndPoint = null)
+        {
+            _restRequest = new RestRequest(optionalEndPoint, Method.Get);
+            _restRequest.AddHeader("Accept", "application/json");
+            return _restRequest;
+        }
+
+        public static RestRequest CreatePostRequest(string jsonString)
+        {
+            _restRequest = new RestRequest(Method.Post);
+            _restRequest.AddHeader("Accept", "application/json");
+            _restRequest.AddParameter("application/json", jsonString, ParameterType.RequestBody);
+            return _restRequest;
+        }
+
+        public static RestRequest CreatePutRequest(string jsonString)
+        {
+            _restRequest = new RestRequest(Method.Put);
+            _restRequest.AddHeader("Accept", "application/json");
+            _restRequest.AddParameter("application/json", jsonString, ParameterType.RequestBody);
+            return _restRequest;
+        }
+
+        public static RestRequest CreatePatchRequest(string jsonString)
+        {
+            _restRequest = new RestRequest(Method.Patch);
+            _restRequest.AddHeader("Accept", "application/json");
+            _restRequest.AddParameter("application/json", jsonString, ParameterType.RequestBody);
+            return _restRequest;
+        }
+
+        public static RestRequest CreateDeleteRequest()
+        {
+            _restRequest = new RestRequest(Method.Delete);
+            _restRequest.AddHeader("Accept", "application/json");
+            return _restRequest;
+        }
+
+        public static RestResponse GetResponse(RestClient restClient, RestRequest request)
+        {
+            return restClient.Execute(request);
+        }
+
+
+    }
+
+}
---
+++ SeleniumAdvantage_Day3/OneforAllFrameWork/UnitTestSetup/NUnitTestSetup.cs
@@ -0,0 +1,48 @@
+﻿using CoreFramework.DriverCore;
+using CoreFramework.Reporter;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using OpenQA.Selenium;
+
+namespace CoreFramework.UnitTestSetup
+{
+    [TestFixture]
+    public class NUnitTestSetup
+    {
+        public IWebDriver? _driver;
+        public WebDriverAction? driverBaseAction;
+
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            
+            HtmlReport.createTest(TestContext.CurrentContext.Test.ClassName);
+        }
+        [SetUp] 
+        public void SetUp()
+        {
+         
+            HtmlReport.createNode(TestContext.CurrentContext.Test.ClassName, TestContext.CurrentContext.Test.Name);
+            WebDriverManager_.InitDriver("chrome", 1920, 1080);
+            _driver = WebDriverManager_.GerCurrentDriver();
+           
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            _driver?.Quit();
+            TestStatus testStatus = TestContext.CurrentContext.Result.Outcome.Status;
+            if (testStatus.Equals(TestStatus.Passed))
+            {
+                TestContext.WriteLine("passed");
+            }
+            else if (testStatus.Equals(TestStatus.Failed))
+            {
+                TestContext.WriteLine("failed");
+            }
+            HtmlReport.flush();
+        }
+    }
+}
