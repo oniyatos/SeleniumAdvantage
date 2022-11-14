ef38d8f462bd1b173608a46a26e19c7c8c8cb067
Nghiem Van Long <88993999+oniyatos@users.noreply.github.com>
2022-11-14 09:21:06 +0700

    Update for API Request/Response and fix Reporter
    Still missing Test Case for API and mark up, will add later

---
+++ OneforAllFrameWork/CoreFrameWork/WebDriverManager.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneforAllFrameWork.CoreFrameWork
+{
+    internal class WebDriverManager_
+    {
+        private static AsyncLocal<IWebDriver> driver = new AsyncLocal<IWebDriver>();
+        public static void InitDriver(String Browser, int Width, int Height)
+        {
+            IWebDriver newDriver = null;
+            newDriver = WebDriverCreator.CreateLocalDriver(Browser, Width, Height);
+            if (newDriver == null)
+                throw new Exception($"{Browser} browser is not supported");
+            driver.Value = newDriver;
+        }
+        public static IWebDriver GetCurrentDriver()
+        {
+            return driver.Value;
+        }
+
+        public static void CloseDriver()
+        {
+            if (driver.Value != null)
+            {
+                driver.Value.Quit();
+                driver.Value.Dispose();
+            }
+        }
+    }
+
+}
---
+++ OneforAllFrameWork/Utilities/FilePath.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System.Reflection;
+
+namespace CoreFramework.Utilities
+{
+    internal class FilePath
+    {
+        public static void CreateFolder(string path) { Directory.CreateDirectory(path); }
+        public static void DeleteFolder(string path) { Directory.Delete(path); }
+        public static void CreateFile(string path) { File.Create(path); }
+        public static void DeleteFile(string path) { File.Delete(path); }
+        public static string GetCurrentDirectoryPath()
+        {
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\..\\..\\..";
+            TestContext.Progress.WriteLine(path);
+            return path;
+        }
+        public static void CreateIfNotExists(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+    }
+}
